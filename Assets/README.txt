663410300baefa2cf047748004c88a4f429d448c
Ben16 <benfickes88@gmail.com>
2025-04-16 20:54:38 -0700

    Initial files with a few minor additions to player movement

---
+++ Assets/README.txt
@@ -0,0 +1,29 @@
+Welcome to the Tic Toc Unity 3D test!
+
+This project will assess your understanding of basic 3D concepts in Unity and your ability to implement gameplay mechanics such as movement, jumping and collision detection.
+
+Your goal is to create a small interactive 3D environment in Unity that demonstrates the following gameplay mechanics:
+- Player Movement: Implement basic character controls for movement in 3D space (forward, backward, left, right and jump).
+- Coin collecting: Passing over coins should destroy them and update a UI element showing how many coins the player has collected so far.
+- Click-to-Fire Projectile: Player can fire a projectile while in first-person view.
+	- Player should only be able to fire AFTER grabbing the gun on top of the platform.
+	- Targets should react on impact (either by destroying them, playing an animation or spawning a visual effect)
+	- Note: Included camera script can switch between third and first person view by right clicking.
+	- Note: Projectile can be represented by a simple shape (e.g., a sphere or cube) or visual effect.
+
+
+Extra requirements
+- Use latest Unity LTS version to import the project.
+- Please ensure that your code is clear and well-documented.
+- Demonstrate a solid grasp of 3D space, physics, and model-view separation.
+- Package your project files for submission, including a short README explaining your implementation approach and any assumptions you made.
+
+Your submission will be evaluated based on:
+- Functionality: Does your project fulfil the outlined requirements?
+- Code Quality: Is your code efficient, clean, and well-commented?
+- Problem-Solving: How effectively do you handle mechanics such as movement, jumping, and projectile trajectory?
+- Flexibility: Can parameters be tweaked to adjust game feel?
+
+Once you’ve completed the assignment please upload your project, including the README explaining your implementation, to a public GitHub repository, archive it and send us a link by the provided deadline.
+
+If you have any questions or need clarification, don’t hesitate to reach out!
---
+++ Assets/Scripts/ExamplePlayer.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+
+namespace KinematicCharacterController.Examples
+{
+    public class ExamplePlayer : MonoBehaviour
+    {
+        public Transform Character;
+        public ExampleCharacterCamera CharacterCamera;
+
+        public float Speed = 5.0f;
+
+        private const string MouseXInput = "Mouse X";
+        private const string MouseYInput = "Mouse Y";
+        private const string MouseScrollInput = "Mouse ScrollWheel";
+
+        private const string MovementHorizontal = "Horizontal";
+        private const string MovementVertical = "Vertical";
+
+        private void Start()
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+
+            // Tell camera to follow transform
+            CharacterCamera.SetFollowTransform(Character);
+
+            // Ignore the character's collider(s) for camera obstruction checks
+            CharacterCamera.IgnoredColliders.Clear();
+            CharacterCamera.IgnoredColliders.AddRange(Character.GetComponentsInChildren<Collider>());
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+
+            HandleMovement();
+        }
+
+        private void LateUpdate()
+        {
+            HandleCameraInput();
+        }
+
+        private void HandleMovement()
+        {
+            float moveHorizontal = Input.GetAxisRaw(MovementHorizontal);
+            float moveVertical = Input.GetAxisRaw(MovementVertical);
+
+            Vector3 direction = new Vector3(moveHorizontal, 0, moveVertical);
+
+            Character.position += direction * Time.deltaTime * Speed;
+        }
+
+        private void HandleCameraInput()
+        {
+            // Create the look input vector for the camera
+            float mouseLookAxisUp = Input.GetAxisRaw(MouseYInput);
+            float mouseLookAxisRight = Input.GetAxisRaw(MouseXInput);
+            Vector3 lookInputVector = new Vector3(mouseLookAxisRight, mouseLookAxisUp, 0f);
+
+            // Prevent moving the camera while the cursor isn't locked
+            if (Cursor.lockState != CursorLockMode.Locked)
+            {
+                lookInputVector = Vector3.zero;
+            }
+
+            // Input for zooming the camera (disabled in WebGL because it can cause problems)
+            float scrollInput = -Input.GetAxis(MouseScrollInput);
+#if UNITY_WEBGL
+        scrollInput = 0f;
+#endif
+
+            // Apply inputs to the camera
+            CharacterCamera.UpdateWithInput(Time.deltaTime, scrollInput, lookInputVector);
+
+            // Handle toggling zoom level
+            if (Input.GetMouseButtonDown(1))
+            {
+                CharacterCamera.TargetDistance = (CharacterCamera.TargetDistance == 0f) ? CharacterCamera.DefaultDistance : 0f;
+            }
+        }
+    }
+}
